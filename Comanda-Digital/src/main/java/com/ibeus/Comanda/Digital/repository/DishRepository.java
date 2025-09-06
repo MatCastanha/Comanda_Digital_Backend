7a6d32a671718fae546144163900b9fd02961212
Matheus Castanha Rodrigues <matheuscastrod33@gmail.com>
2025-09-06 17:35:03 -0300

    Código Base

---
+++ Comanda-Digital/src/main/java/com/ibeus/Comanda/Digital/repository/DishRepository.java
@@ -0,0 +1,13 @@
+package com.ibeus.Comanda.Digital.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ibeus.Comanda.Digital.model.Dish;
+import java.util.List;
+
+
+
+public interface DishRepository extends JpaRepository<Dish, Long> {
+
+    List<Dish> findByNameStartingWithIgnoreCase(String name);
+
+}
